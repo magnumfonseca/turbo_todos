45fe06fe63a0cc42a482e09bf9f30f45ca301e95
Magnum Fonseca <magnum.td.fonseca@gmail.com>
2022-06-01 20:48:52 -0300

    fix broadcast id

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,13 +2,13 @@ class Todo < ApplicationRecord
   belongs_to :user
 
   after_update_commit do
-    broadcast_append_to("todos") if self.complete?
-    broadcast_prepend_to("todos") if self.incomplete?
+    broadcast_append_to("broadcast_to_user_#{self.user_id}") if self.complete?
+    broadcast_prepend_to("broadcast_to_user_#{self.user_id}") if self.incomplete?
   end
 
   after_create_commit do
     broadcast_prepend_to(
-      "notices",
+      "broadcast_to_user_#{self.user_id}",
       target: 'notices',
       partial: "notices/notice",
       locals: { todo: self }
